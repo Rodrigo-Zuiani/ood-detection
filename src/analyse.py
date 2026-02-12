596b00a1549564b14fde390d0a66d73b32e4f99f
gatrellesse <gabrielbtrellesse@gmail.com>
2026-02-12 19:53:49 +0100

    feat: nc2 and nc1

--- src/analyse.py
+++ src/analyse.py
@@ -4,6 +4,7 @@ from .models.resnet import BasicBlock, ResNet18
 from .dataset.cifar100 import get_cifar100_loaders
 import random
 import numpy as np
+import os 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = ResNet18(block=BasicBlock, num_blocks=[2, 2, 2, 2], num_classes=100).to(device)
@@ -34,19 +35,103 @@ trainloader, testloader, classes = get_cifar100_loaders(
 
 features = []
 labels = []
-checkpoint = torch.load("src/checkpoints/resnet18_cifar100_epoch_020.pt", weights_only=True)
-model.load_state_dict(checkpoint['model_state'])
+epochs = []
+checkpoint_folder = "src/checkpoints"
+cp_file_names = os.listdir(checkpoint_folder)
 
-model.eval()
+print(cp_file_names)
+distance_means_dict = {}
+std_means_dict = {}
+wt_class_var = {}
 
-with torch.no_grad():
-    for x, y in trainloader:
-        x = x.to(device)
-        z = model.forward_features(x)  # penultimate features
-        features.append(z.cpu())
-        labels.append(y)
+for cp_file in cp_file_names:
+    checkpoint_file = os.path.join(checkpoint_folder, cp_file)
+    checkpoint = torch.load(checkpoint_file, weights_only=True)
+    model.load_state_dict(checkpoint['model_state'])
+    epoch = checkpoint["epoch"]
+    epochs.append(epoch)
+    model.eval()
 
-features = torch.cat(features)  # [N, d] [50000, 3, 32, 32]
-print(features.size())
-labels = torch.cat(labels)      # [N] ([50000, 3, 32, 32]
-print(features.size())
+    with torch.no_grad():
+        for x, y in trainloader:
+            x = x.to(device)
+            z = model.forward_features(x)  # penultimate features before FC
+            features.append(z.cpu())
+            labels.append(y)
+
+    features = torch.cat(features)  # [N, d] [50000, 512] for cifar-100
+    labels = torch.cat(labels)      # [N] ([50000]
+
+
+    # Class Means Distances (NC2) --> Simplex ETF Structure
+    num_classes = labels.max().item() + 1
+    d = features.size(1)
+
+    class_means = torch.zeros(num_classes, d) # [100, 512]  
+
+    for c in range(num_classes):
+        idx = labels == c               # boolean mask : True where labels is class C, otherwise False
+        if len(sum(idx) == 0):
+            continue
+        class_means[c] = features[idx].mean(dim=0)
+
+    dist_matrix = torch.cdist(class_means, class_means, p=2) # Class mean distance for 1 epoch [C, C]
+    print(f"dist_matrix: {dist_matrix}")
+    pairwise_distances = dist_matrix[torch.triu(torch.ones_like(dist_matrix), diagonal=1).bool()] # Upper triangle
+    std_dist  = pairwise_distances.std() # Since everyone tends to the same value: std-->0
+    print(f"pairwise: {pairwise_distances}")
+    print(f"std dist: {std_dist}")
+    # Within class variance (NC1) --> Variability Collapse Sum(W) = 0
+    within_class_var = 0.0
+
+    for c in range(num_classes):
+        idx = labels == c
+        z_c = features[idx]                 # [N_c, d]
+        mu_c = class_means[c]               # [d]
+        within_class_var += ((z_c - mu_c)**2).sum()
+
+    within_class_var /= features.size(0)
+    
+    wt_class_var[epoch] = within_class_var
+    distance_means_dict[epoch] = pairwise_distances
+    std_means_dict[epoch] = std_dist
+
+ordered_std = {k:v for k,v in sorted(std_means_dict.items(), key=lambda item:item[0])}
+ordered_pairwise = {k:v for k,v in sorted(distance_means_dict.items(), key=lambda item:item[0])}
+ordered_var = {k:v for k,v in sorted(wt_class_var.items(), key=lambda item:item[0])}
+
+stds = list(ordered_std.values())
+wt_var = list(ordered_var.values())
+pairs = list(ordered_pairwise.values())
+epochs.sort()
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.plot(epochs, wt_var, label="Variance distance")
+plt.plot(epochs, stds, label="Std distance")
+plt.xlabel("Epoch")
+plt.ylabel("Distance")
+plt.title("Class mean distances over training")
+plt.legend()
+plt.show()
+
+plot_path = "src/plots/evolution_dist.png"
+plt.tight_layout()
+plt.savefig(plot_path, dpi=300)
+plt.close()
+
+
+data = [d.cpu().numpy() for d in pairs]
+
+plt.figure(figsize=(10,4))
+plt.boxplot(data, positions=epochs, showfliers=False)
+plt.xlabel("Epoch")
+plt.ylabel("Class mean distance")
+plt.title("Distribution of class mean distances")
+plt.show()
+
+
+plot_path = "src/plots/simplex.png"
+plt.tight_layout()
+plt.savefig(plot_path, dpi=300)
+plt.close()
